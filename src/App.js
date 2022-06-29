a89b21540a5a0fe0c0ba6749393aef22c5da831f
Zahid Ali <zahidalichandio240@gmail.com>
2022-06-29 14:06:10 +0500

    Learning REact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+function myFunct(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+let user = {
+  firstName: "Ali",
+  lastName: "Ahmed"
+}
+let count = 0;
+let counter = (() => {
+  return ++count;
+}, 100);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <nav id="navbar">
+        <li className="item" id="item1"><a href="#">Home</a></li>
+        <li className="item" id="item2"><a href="#">Services</a></li>
+        <li className="item" id="item3"><a href="#">Contact Us</a></li>
+        <li className="item" id="item4"><a href="#">About Us</a></li>
+      </nav>
+      <div className="heading">
+        MyFirstHeading
+      </div>
+      <div className="user">
+        Hello,{myFunct(user)}
+      </div>
+
+      <div className="count">
+        {counter}
+      </div>
     </div>
+    </>
   );
 }
 
